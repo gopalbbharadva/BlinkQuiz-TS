378f5a2ec99d55a969203cd11f02c8c5c823faf9
gopal <gopalbbharadva@gmail.com>
2022-05-09 10:24:10 +0530

    Export: Barrel Exports

---
+++ src/contexts/contextExport.ts
@@ -0,0 +1,5 @@
+import { useAuth } from "./AuthContext";
+import { useQuizQuestion } from "./QuestionContext";
+import { useQuiz } from "./QuizContext";
+
+export { useQuizQuestion, useAuth, useQuiz };
